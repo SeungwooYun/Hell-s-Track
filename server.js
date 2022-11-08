19450d51acef2b536aba9803dcfdd9737c0c7fc3
Seungwoo Yun <yoooonseungwoo@gmail.com>
2022-11-09 02:03:52 +0900

    implemented recording reps and sets, need to work on DB

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.set('view engine', 'ejs');
 
 
 // adding workout name - array
-let list_contents = []
+let workoutList = []
+// 변수명 변경 시작
+let kgsAndReps = []
+let workoutTitle = ''
 
 app.get('/', (req, res) => {
     const date = new Date();
@@ -30,15 +33,15 @@ app.get('/', (req, res) => {
 
     res.render('list', {
         listTitle: formattedDate,
-        list_contents: list_contents
+        workoutList: workoutList
     });
 });
 
 
 app.post('/', (req, res) => {
     console.log(req.body)
-    let list_content = req.body.list_content
-    list_contents.push(list_content)
+    let workoutList_element = req.body.workoutList_element
+    workoutList.push(_.startCase(workoutList_element))
     res.redirect('/')
 }
 
@@ -47,23 +50,41 @@ app.post('/', (req, res) => {
 
 app.get('/workout', (req, res) => {
     res.render('workout', {
-        workoutTitle: "Work Out"
+        workoutTitle: workoutTitle,
+        kgsAndReps: kgsAndReps
     })
 })
 
+
 app.get('/workout/:anyname', (req, res) => {
     const loweredParams = _.lowerCase(req.params.anyname)
-    list_contents.forEach((content) => {
+    workoutList.forEach((content) => {
         const loweredListContent = _.lowerCase(content)
         if (loweredParams === loweredListContent) {
             res.render('workout', {
-                workoutTitle: content
+                workoutTitle: _.startCase(content),
+                kgsAndReps: kgsAndReps
             })
-        } else {
-            res.send("오류페이지다잉")
         }
     })
 })
+
+
+
+app.post('/workout', (req, res) => {
+    const kgAndRep = [req.body.kg, req.body.rep]
+    kgsAndReps.push(kgAndRep)
+    workoutTitle = req.body.workoutTitle
+    console.log(req.body)
+    res.redirect('/workout')
+})
+// :anyone 라우트로 redirect 하는 게 불가능하지 않나? 
+// 그럼 어떻게 하면 좋을까. 어차피 form 안에 모든 내용들 담겨있을 거고 리스트까지 있으니까
+// 새로 만들어진 라우트는 실제로 만들어지게 만드는 게 나으려나? 라우트가 엄청 많아지겠지만.
+
+// 그냥 workout route 에다가 렌더링하면 되겠구나 와 진짜 예상치못했다
+
+
 
 app.get('/about', (req, res) => {
     res.render('about')
